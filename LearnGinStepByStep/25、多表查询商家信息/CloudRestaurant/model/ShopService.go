4e081351a7692312e5ba2662e3947f2609db233a
Starry <codeprince2020@163.com>
2023-01-27 23:47:45 +0800

    refactor: 结构调整

--- LearnGinStepByStep/25、多表查询商家信息/CloudRestaurant/model/ShopService.go
+++
@@ -1,7 +0,0 @@
-package model
-
-//商铺服务数据对应表
-type ShopService struct {
-	ShopId    int64 `xorm:"pk not null" json:"shop_id"`
-	ServiceId int64 `xorm:"pk not null" json:"service_id"`
-}
--- LearnGinStepByStep/25、多表查询商家信息/CloudRestaurant/model/SmsCode.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type SmsCode struct {
-	Id         int64  `xorm:"pk autoincr" json:"id"`
-	Phone      string `xorm:"varchar(11)" json:"phone"`
-	BizId      string `xorm:"varchar(30)" json:"biz_id"`
-	Code       string `xorm:"varchar(6)" json:"code"`
-	CreateTime int64  `xorm:"bigint" json:"create_time"`
-}
